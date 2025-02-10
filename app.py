bc81b1ca983d88d110275346fbb04ab3ace94836
pranavGenAI <167668235+pranavGenAI@users.noreply.github.com>
2025-02-10 14:29:11 +0530

    app.py

--- app.py
+++ app.py
@@ -16,6 +16,7 @@ from agno.models.groq import Groq
 from agno.tools.duckduckgo import DuckDuckGoTools
 from agno.tools.newspaper4k import Newspaper4kTools
 import json
+import time
 
 # Fetch API keys from Streamlit secrets
 groq_api_key = "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"
@@ -187,31 +188,37 @@ def main(address):
 
     if user_question:
         with st.spinner("Thinking..."):
+            time.sleep(1.5)
             with st.spinner("Orchestrator Agent Invoked"):
+                time.sleep(1.5)
+                with st.spinner("Orchestrator Agent Monitoring"):
+                    with st.spinner("Summarizer Agent Invoked"):
+                        time.sleep(1.5)
+                        with st.spinner("Utility Agents Invoked"):
+                            time.sleep(1.5)
+                    with st.spinner("Recommender Agent Invoked"):
+                        time.sleep(1.5)
+                        with st.spinner("Utility Agents Invoked"):
+                            time.sleep(1.5)
+            with st.spinner("Orchestrator Ensuring Response Completeness"):
+                time.sleep(1.5)
+        with st.spinner("Chatbot loading response..."):
+                        
                 # Generate the steps and content
                 generated_steps = generate_steps(user_question, model)
                 generated_text = generate_content(user_question, model, address)
-
+                
+        if generated_steps:
+            # Create a collapsible box to display steps
+            with st.expander("Steps Performed"):
+                st.write(generated_steps)
         # Display the generated content
         if generated_text:
             st.markdown("### ReliefBot:")
             st.write(generated_text)
 
         # Display the steps in a collapsible box
-        if generated_steps:
-            try:
-                # Assuming generated_steps is a JSON string, let's parse it
-                step_list = json.loads(generated_steps)
-            except json.decoder.JSONDecodeError as e:
-                st.error(f"Error parsing JSON: {str(e)}")
-                step_list = []
-
-            # Create a collapsible box to display steps
-            with st.expander("Steps to Process the Request"):
-                for step in step_list:
-                    st.write(f"Step {step['step_number']}: {step['step_description']}")
-                    time.sleep(1.5)  # Delay between each step before showing the next one
- 
+       
 # Main app flow
 if __name__ == "__main__":
     main(address)
