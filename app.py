2ceca4e1ea7e60b73a4f4c62313aa6e72bcb242c
pranavGenAI <167668235+pranavGenAI@users.noreply.github.com>
2025-02-10 08:55:18 +0530

    app.py

--- app.py
+++ app.py
@@ -18,15 +18,12 @@ else:
 latitude = lat
 longitude = lon
 
-if st.button("Get Address"):
-    api_key = "ab5b5ba90347427cb889b0b4c136e0bf"
-    url = f"https://api.opencagedata.com/geocode/v1/json?q={latitude}+{longitude}&key={api_key}"
-
-    response = requests.get(url)
-    data = response.json()
-
-    if response.status_code == 200 and data["results"]:
-        address = data["results"][0]["formatted"]
-        st.success(f"Address: {address}")
-    else:
-        st.warning("Address not found.")
+api_key = "ab5b5ba90347427cb889b0b4c136e0bf"
+url = f"https://api.opencagedata.com/geocode/v1/json?q={latitude}+{longitude}&key={api_key}"
+response = requests.get(url)
+data = response.json()
+if response.status_code == 200 and data["results"]:
+    address = data["results"][0]["formatted"]
+    st.success(f"Address: {address}")
+else:
+    st.warning("Address not found.")
